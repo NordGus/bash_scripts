59ed229afe98f8c30cee34c4ad91936650193910
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-03-08 15:31:18 +0100

    adding echo messages

--- desktop/reset_audio_sources.sh
+++ desktop/reset_audio_sources.sh
@@ -2,6 +2,8 @@
 
 # Simple script to update audio sources in Pop_OS!
 
+echo "Reseting Audio Sources."
 systemctl --user restart pulseaudio
 rm -r ~/.config/pulse
 pulseaudio -k
+echo "Audio Sources Reseted."
