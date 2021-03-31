75228142de2301a6183c32ace07d39e572aaa953
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2021-03-31 21:02:05 +0200

    setups for node development

--- initialize_env.sh
+++ initialize_env.sh
@@ -3,3 +3,5 @@
 
 chmod 755 $BASH_SCRIPTS_PATH/desktop/gh_init.sh
 chmod 755 $BASH_SCRIPTS_PATH/desktop/reset_audio_sources.sh
+chmod 755 $BASH_SCRIPTS_PATH/desktop/init_phaser
+chmod 755 $BASH_SCRIPTS_PATH/desktop/install_nvm
